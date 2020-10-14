5a04f87f93c02586760234ac9287e84d79961018
Hamza Anis <hamzaanis9514@gmail.com>
2020-10-14 18:04:14 +0500

    Working particles.

--- static/js/main.js
+++ static/js/main.js
@@ -1,24 +1,24 @@
-jQuery(document).ready(function($) {
- 
-    $(window).scroll(function() {
+jQuery(document).ready(function ($) {
+
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
         }
     });
-    $('.back-to-top').click(function() {
+    $('.back-to-top').click(function () {
         $('html, body').animate({
             scrollTop: 0
         }, 1500, 'easeInOutExpo');
         return false;
     });
- 
+
     $("#header").sticky({
         topSpacing: 0,
         zIndex: '50'
     });
- 
+
     $("#intro-carousel").owlCarousel({
         autoplay: true,
         dots: false,
@@ -26,16 +26,16 @@ jQuery(document).ready(function($) {
         animateOut: 'fadeOut',
         items: 1
     });
- 
+
     new WOW().init();
- 
+
     $('.nav-menu').superfish({
         animation: {
             opacity: 'show'
         },
         speed: 400
     });
- 
+
     if ($('#nav-menu-container').length) {
         var $mobile_nav = $('#nav-menu-container').clone().prop({
             id: 'mobile-nav'
@@ -49,19 +49,19 @@ jQuery(document).ready(function($) {
         $('body').append('<div id="mobile-body-overly"></div>');
         $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
 
-        $(document).on('click', '.menu-has-children i', function(e) {
+        $(document).on('click', '.menu-has-children i', function (e) {
             $(this).next().toggleClass('menu-item-active');
             $(this).nextAll('ul').eq(0).slideToggle();
             $(this).toggleClass("fa-chevron-up fa-chevron-down");
         });
 
-        $(document).on('click', '#mobile-nav-toggle', function(e) {
+        $(document).on('click', '#mobile-nav-toggle', function (e) {
             $('body').toggleClass('mobile-nav-active');
             $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
             $('#mobile-body-overly').toggle();
         });
 
-        $(document).click(function(e) {
+        $(document).click(function (e) {
             var container = $("#mobile-nav, #mobile-nav-toggle");
             if (!container.is(e.target) && container.has(e.target).length === 0) {
                 if ($('body').hasClass('mobile-nav-active')) {
@@ -74,8 +74,8 @@ jQuery(document).ready(function($) {
     } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
         $("#mobile-nav, #mobile-nav-toggle").hide();
     }
- 
-    $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
+
+    $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             if (target.length) {
@@ -108,7 +108,7 @@ jQuery(document).ready(function($) {
         }
     });
 
- 
+
     $('.portfolio-popup').magnificPopup({
         type: 'image',
         removalDelay: 300,
@@ -120,12 +120,12 @@ jQuery(document).ready(function($) {
             enabled: true,
             duration: 300,
             easing: 'ease-in-out',
-            opener: function(openerElement) {
+            opener: function (openerElement) {
                 return openerElement.is('img') ? openerElement : openerElement.find('img');
             }
         }
     });
- 
+
     $(".testimonials-carousel").owlCarousel({
         autoplay: true,
         dots: true,
@@ -142,7 +142,7 @@ jQuery(document).ready(function($) {
             }
         }
     });
- 
+
     $(".clients-carousel").owlCarousel({
         autoplay: true,
         dots: true,
@@ -159,6 +159,117 @@ jQuery(document).ready(function($) {
             }
         }
     });
+    /* ---- particles.js config ---- */
+    const particlesJSON = {
+        "particles": {
+            "number": {
+                "value": 80,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#FFFFFF"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#FFFFFF"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#FFFFFF",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 140,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        }
+    }
+    particlesJS("particles-js", particlesJSON)
 
 
 });
